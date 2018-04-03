1592b015bddcd1fb88971a4f30576bf8330ebd9f
leeks0524 <leeks0524@hotmail.com>
2018-04-03 08:44:57 -0400

    Adding background and pde file

--- src/MathmaticalSheep.java
+++ src/MathmaticalSheep.java
@@ -5,7 +5,7 @@ import java.lang.String;
 
 public class MathmaticalSheep extends PApplet {
     public void settings() {
-        size(640, 360);
+        size(600, 430);
     } //size of game screen
 
     static public void main(String[] passedArgs) {
@@ -18,18 +18,20 @@ public class MathmaticalSheep extends PApplet {
     }
     //SETTING global variables
     NumMon[] mons; //floating number monsters on the screen
-    float diameter = 40; //diameter of number monster
+    float diameter = 30; //diameter of number monster
     float playerDiameter = 50; //detecting size of llama to later calculate when lama reach number monster
     Player player;
-    ArrayList<Integer> primeNumList = new ArrayList<>(Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199));
+    //ArrayList<Integer> primeNumList = new ArrayList<>(Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199));
     boolean playerAlive; //llama or explosion image ~ numbers moving or numbers stop moving
     boolean historyModeOn;
     int score;
     ArrayList <Integer> scoreHistory;
     int MAX_SCORE_HISTORY = 5; //top five scores are shown in history
-    int counter; // switching llama image to make it look walking
+    int singleGameCounter; // switching llama image to make it look walking
     PImage[] llama;
     PImage[] explosion;
+    PImage backgroundImage;
+    String[] suffixes = {"st", "nd", "rd", "th", "th"};
 
     public void setup() {
         mons = new NumMon[4]; //only four number monsters are shown in screen - array of four number monsters
@@ -45,13 +47,14 @@ public class MathmaticalSheep extends PApplet {
             scoreHistory.add(0);
         }
         textAlign(CENTER);
-        counter = 0;
+        singleGameCounter = 0;
         llama = new PImage[4];
         explosion = new PImage[4];
         for (int i = 0; i < llama.length; i++) {
             llama[i] = loadImage("/img/llama_" + i + ".png");
             explosion[i] = loadImage("/img/explosion_" + i + ".png");
         }
+        backgroundImage = loadImage("/img/cyberpunk-street.png");
     }
 
     public void reset() {
@@ -63,20 +66,26 @@ public class MathmaticalSheep extends PApplet {
             mons[i] = new NumMon(random((height / 4 * i + diameter / 2), (height / 4 * (i + 1) - diameter / 2)), (int) random(200));
         }
         player = new Player();
-        counter = 0;
+        singleGameCounter = 0;
     }
 
     public void draw() {
         if (historyModeOn) {
-            background(132);
+            background(32);
             textSize(40);
             fill(23, 140, 210);
-            text("Score Board", width / 2, 50);
+            text("Score Board", width / 2, 80);
             for (int i = 0; i < scoreHistory.size(); i++) {
-                text((i + 1) + ". " + scoreHistory.get(i), width / 2, 50 + 40 * (i + 1));
+                text((i + 1) + suffixes[i] + ".   " + scoreHistory.get(i), width / 2, 100 + 50 * (i + 1));
             }
         } else {
-            background(0);
+            int backgroundX = singleGameCounter % backgroundImage.width;
+            if (backgroundImage.width - backgroundX > width) {
+                copy(backgroundImage, backgroundX, 0, width, height, 0, 0, width, height);
+            } else {
+                copy(backgroundImage, backgroundX, 0, backgroundImage.width - backgroundX, height, 0, 0, backgroundImage.width - backgroundX, height);
+                copy(backgroundImage, 0, 0, width - backgroundImage.width + backgroundX, height, backgroundImage.width - backgroundX, 0, width - backgroundImage.width + backgroundX, height);
+            }
             for (int i = 0; i < mons.length; i++) {
                 if (mons[i].getX() < diameter * -1) {
                     mons[i] = new NumMon(random((height / 4 * i + diameter / 2), ((height / 4 * (i + 1)) - diameter / 2)), (int) random(200));
@@ -88,7 +97,7 @@ public class MathmaticalSheep extends PApplet {
                 mons[i].show();
                 mons[i].collisionDetection(player);
             }
-            player.show(counter);
+            player.show(singleGameCounter);
 
             textSize(30);
             fill(0, 102, 153);
@@ -97,11 +106,8 @@ public class MathmaticalSheep extends PApplet {
             text("Press 'r' to restart the game, and 's' to see the scoreboard.", width / 2, height - 20);
 
         }
-        if (counter < 79) {
-            counter = counter + 1;
-        } else {
-            counter = 0;
-        }
+
+        singleGameCounter++;
     }
 
     public void keyPressed() {
@@ -117,8 +123,12 @@ public class MathmaticalSheep extends PApplet {
                 player.move(0, -5);
             } else if(keyCode == DOWN) {
                 player.move(0, 5);
+            } else if(keyCode == LEFT) {
+                player.move(-5, 0);
+            } else if(keyCode == RIGHT) {
+                player.move(5, 0);
             }
-            player.show(counter);
+            player.show(singleGameCounter);
         }
     }
 
@@ -137,6 +147,7 @@ public class MathmaticalSheep extends PApplet {
 
         void show(int counter) {
             //ellipse(x, y, playerDiameter,playerDiameter);
+            counter = counter % 80;
             if (playerAlive) {
                 image(llama[counter / 20], x - playerDiameter / 2, y - playerDiameter / 2, playerDiameter, playerDiameter);
             } else {
@@ -171,14 +182,13 @@ public class MathmaticalSheep extends PApplet {
 
         void show() {
             if (visible) {
-                fill(255);
-                stroke(138, 43);
+                fill(244, 244, 113);
+                stroke(253,35, 21)  ;
                 ellipse(x, y, diameter, diameter);
-                textSize(20);
-                fill(0, 102, 153);
-                text(sign, x,  y+ 8);
+                textSize(14);
+                fill(171, 92, 169);
+                text(sign, x,  y+ 5);
             }
-
         }
 
         float getX() {
@@ -190,7 +200,7 @@ public class MathmaticalSheep extends PApplet {
             if (distance < (diameter + playerDiameter) / 2) {
                 visible = false;
                 if (!collected) {
-                    if (primeNumList.contains(sign)) {
+                    if (true) {
                         score = score + 1;
                     } else {
                         playerAlive = false;
